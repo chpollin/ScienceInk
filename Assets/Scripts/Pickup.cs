ec0d10e37981c380e9c26b29dd5d09c8568d25cc
Christopher Pollin <christopher.pollin@uni-graz.at>
2020-07-22 15:26:08 +0200

    add

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public AudioClip soundEffect;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerManager manager = collision.GetComponent<PlayerManager>();
+        if (manager)
+        {
+            bool pickedUp = manager.PickupItem(gameObject);
+            if(pickedUp)
+            {
+                RemoveItem();
+            }
+        }
+    }
+
+    public void RemoveItem()
+    {
+        
+        AudioSource.PlayClipAtPoint(soundEffect, transform.position);
+        Destroy(gameObject);
+    }
+
+
+
+
+}
