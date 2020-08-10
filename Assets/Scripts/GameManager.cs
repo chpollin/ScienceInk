a988123790134390993b8271276a87c4c1251bde
Christopher Pollin <christopher.pollin@uni-graz.at>
2020-08-10 12:18:31 +0200

    Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false; 
+
+    public float restartDelay = 1f;
+
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level 1");
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+  
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
