cc3c1e7973c04dc720cb2b95c23d79a5e29476ba
Christopher Pollin <christopher.pollin@uni-graz.at>
2020-08-17 13:52:48 +0200

    DialogSystem

---
+++ Assets/Scripts/Dialog/CloseDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDialog : MonoBehaviour
+{
+    public GameObject dialogPanel;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(dialogPanel.activeSelf == true)
+        {
+            dialogPanel.SetActive(false);
+
+        } 
+    }
+}
---
+++ Assets/Scripts/Dialog/OpenDialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDialog : MonoBehaviour
+{
+    public GameObject dialogPanel;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            Debug.Log("trigger");
+            if(dialogPanel.activeSelf == true)
+            {
+              Debug.Log("Dialog war noch nciht geschlossen");  
+            }
+            else
+            {
+                dialogPanel.SetActive(true);
+            }
+            
+            
+        }
+    }
+}
