8d9cf33e2076cb42918c0e5f0a4d0a18fca1da14
chpollin <christopher.pollin@uni-graz.at>
2020-09-17 14:23:39 +0200

    add slippery physical material to player
    https://answers.unity.com/questions/794668/2d-character-controller-getting-stuck-on-walls.html

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int mandelCount;
+    public Text mandelNumberText;
+    public static PlayerManager instance;
+    int score;
+
+    public bool PickupItem(GameObject obj)
+    {
+        switch(obj.tag)
+        {
+            case "currency":
+                // erhöhe die anzahl der mandeln
+                //mandelCount++;
+                //mandelNumberText.text = "X" + mandelCount.ToString();
+                Debug.Log("Currency picked up");
+                return true;
+                break;
+            case "ingredient":
+                // kochzutaten kommen ins inventar
+                 Debug.Log("Ingredient picked up");
+                return true;
+                break;
+            default:
+                return false;
+                break;
+        }
+
+    }
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int mandelValue)
+    {
+        score += mandelValue;
+        mandelNumberText.text = "X" + score.ToString();
+    }
+}
