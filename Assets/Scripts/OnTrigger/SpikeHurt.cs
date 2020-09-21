ce41e0428414d8bdaaa7c1764be86b582fb365ff
chpollin <christopher.pollin@uni-graz.at>
2020-09-21 09:56:31 +0200

    trigger fix spikes

--- Assets/Scripts/OnTrigger/SpikeHurt.cs
+++ Assets/Scripts/OnTrigger/SpikeHurt.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class SpikeHurt: MonoBehaviour
 {
     public GameManager gameManager;
+    bool _triggered;
 
-    // 
-
+    ////////////////////////////////////////////////
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.gameObject.tag == "Player")
-        {
-            Debug.Log("hurt!!!");
-            LiveCounter.health -= 1;
+        if(_triggered){
+            if(collider.gameObject.tag == "Player")
+            {
+                Debug.Log("hurt!!!");
+                LiveCounter.health -= 1;
 
 
 
+            }
         }
+        _triggered = true;
     }
+
+    ////////////////////////////////////////////////
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+     if (!_triggered)
+     {
+         return;
+     }
+     _triggered = false;
+     Debug.Log("exit");
+    }
+
 }
