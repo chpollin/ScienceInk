ccd9308e8d6ced251a0e2d86d13fe84df1276aab
chpollin <christopher.pollin@uni-graz.at>
2020-09-21 09:21:36 +0200

    fix ontrigger fired twice items

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -9,27 +9,49 @@ public class Pickup : MonoBehaviour
     public Item item;
     public Inventory inventory;
     public QuestLogManager questLogManager;
+    bool _triggered;
+    public int almondValue = 1;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerManager manager = collision.GetComponent<PlayerManager>();
-        if (manager)
-        {
-            bool pickedUp = manager.PickupItem(gameObject);
-            if(pickedUp)
+        // fixing fireing OnTrigget Twice; set a boolean value when triggered
+        if(_triggered){
+            if (manager)
             {
-                RemoveItem();
-                // pick up and add item to the inventory
-                //ToDo Almond has id=100; select by tag would be better
-                if(item_id<99)
+                bool pickedUp = manager.PickupItem(gameObject);
+                if(pickedUp)
                 {
-                    inventory.GiveItem(item_id);
-                    // also add ingredient in the QuestLog
-                    questLogManager.addIngredientToQuest(item_id);
-                    
-                } 
+                    RemoveItem();
+                    // pick up and add item to the inventory
+                    //ToDo Almond has id=100; select by tag would be better
+                    if(item_id<99)
+                    {
+                        inventory.GiveItem(item_id);
+                        // also add ingredient in the QuestLog
+                        questLogManager.addIngredientToQuest(item_id);
+                        
+                    } 
+                    if(item_id == 100)
+                    {
+                        PlayerManager.instance.ChangeScore(almondValue);
+                    }
+                }
+            Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>());    
             }
         }
+         _triggered = true;
+    }
+
+    // fixing fireing OnTrigget Twice; set a boolean value when triggered
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+     if (!_triggered)
+     {
+         return;
+     }
+     _triggered = false;
+     Debug.Log("exit");
     }
 
     public void RemoveItem()
