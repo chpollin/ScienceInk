4cc775c2c25b260d8f3e7391c81efde7b151291b
chpollin <christopher.pollin@uni-graz.at>
2020-09-22 14:27:10 +0200

    add hurt animation ontrigger spikes

--- Assets/Scripts/OnTrigger/SpikeHurt.cs
+++ Assets/Scripts/OnTrigger/SpikeHurt.cs
@@ -6,6 +6,13 @@ public class SpikeHurt: MonoBehaviour
 {
     public GameManager gameManager;
     bool _triggered;
+    public float knockback;
+    public float knockbackLength;
+    public float knockbackCount;
+    public bool knockbackRight;
+
+    private Rigidbody2D rb2D;
+    private Animator animator;
 
     ////////////////////////////////////////////////
     void OnTriggerEnter2D(Collider2D collider)
@@ -15,9 +22,14 @@ public class SpikeHurt: MonoBehaviour
             {
                 Debug.Log("hurt!!!");
                 LiveCounter.health -= 1;
-
-
-
+                rb2D = collider.GetComponent<Rigidbody2D>();
+                animator = collider.GetComponent<Animator>();
+                // move player when he is hurt
+                rb2D.velocity = new Vector2(0.0f,20.0f);
+                // change animation blinking red and jumping
+                //animator.SetTrigger("IsJumping");
+                animator.SetBool("IsHurt", true);
+                animator.SetBool("IsJumping", true);
             }
         }
         _triggered = true;
@@ -28,7 +40,8 @@ public class SpikeHurt: MonoBehaviour
     {
      if (!_triggered)
      {
-         return;
+        animator.SetBool("IsHurt", false);
+        animator.SetBool("IsJumping", false);
      }
      _triggered = false;
      Debug.Log("exit");
