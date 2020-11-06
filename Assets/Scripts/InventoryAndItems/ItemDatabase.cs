09745cbc68e1fe5f178486cb04871e94b3c952d8
chpollin <christopher.pollin@uni-graz.at>
2020-11-06 18:29:17 +0100

    level 3: falldown trigger, ingredients, almonds, level design fixing,

--- Assets/Scripts/InventoryAndItems/ItemDatabase.cs
+++ Assets/Scripts/InventoryAndItems/ItemDatabase.cs
@@ -28,14 +28,25 @@ public class ItemDatabase : MonoBehaviour
         // name like "Bohnenkraut" must be same like the name of the .png
         // ToDo: a number representing the slot in the InventoryPanel
         items = new List<Item>() {
+            // Rezept 1
             new Item(0, "Bohnenkraut", "Lecker Bohnenkraut. Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore", 0),
             new Item(1, "Mangold", "Supi Mangold Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore", 1),
             new Item(2, "Salbei", "Seeliger Salbei Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore", 2),
+            // Rezept 2
             new Item(3, "Reispflanze", "Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore Lorem Ipsum Dolore", 0),
             new Item(4, "Huhn", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 1),
             new Item(5, "Salz", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 2),
             new Item(6, "Pfeffer", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 3),
-            new Item(7, "Erbsen", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 4)
+            new Item(7, "Erbsen", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 4),
+            // Rezept 3
+            new Item(8, "Mehl", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 0),
+            new Item(9, "Schweineschmalz", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 1),
+            //Salz, Inventarposition 2
+            //Pfeffer,Inventarposition 3
+            new Item(10, "Ingwer", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 4),
+            new Item(11, "Pflaume", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 5),
+            new Item(12, "Rotwein", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 6),
+            new Item(13, "Hufeisen", "Lorem Ipsum Dolore Lorem Ipsum Dolore", 7),
         };
     }
 }
