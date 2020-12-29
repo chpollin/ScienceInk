e56d3c0470028d2f57e207a148da2d03910e7f48
chpollin <christopher.pollin@uni-graz.at>
2020-12-29 10:55:40 +0100

    prelude

---
+++ Assets/Scripts/GameManager/PreludeCutscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreludeCutscene : MonoBehaviour
+{
+    //////////////////////////////////
+    public void Update()
+    {
+        StartCoroutine("Wait");
+    }
+ 
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(60f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+     }
+
+}
