6b05c01d21c5ed396963f24974089534ed817659
Christopher Pollin <christopher.pollin@uni-graz.at>
2021-09-08 12:32:05 +0200

    update

---
+++ Assets/Scripts/GameManager/IntroToMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroToMainMenu : MonoBehaviour
+{
+    //////////////////////////////////
+    public void Update()
+    {
+        StartCoroutine("Wait");
+    }
+ 
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void LoadOnClick()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);  
+    }
+
+}
