2af8206ee720af038de74d2b92f266f2007be3ea
chpollin <christopher.pollin@uni-graz.at>
2021-09-09 17:32:00 +0200

    update

---
+++ Assets/soundOnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundOnTrigger : MonoBehaviour
+{
+    public AudioClip clip;
+    public float volume=1;
+    bool _triggered;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(_triggered){
+            if(collider.gameObject.tag == "Player")
+            {
+                 AudioSource.PlayClipAtPoint(clip, transform.position, volume);
+            }
+        }
+        _triggered = true;
+    }
+}
