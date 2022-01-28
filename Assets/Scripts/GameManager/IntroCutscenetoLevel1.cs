4779d7318d35df23bf3ce86924db6d6a907993f9
chpollin <christopher.pollin@uni-graz.at>
2022-01-28 17:28:48 +0100

    update

--- Assets/Scripts/GameManager/IntroCutscenetoLevel1.cs
+++ Assets/Scripts/GameManager/IntroCutscenetoLevel1.cs
@@ -7,20 +7,31 @@ public class IntroCutscenetoLevel1 : MonoBehaviour
 {
 
     public int second = 0;
+    public int audio_1_delay = 0;
+    public int audio_2_delay = 0;
+    public AudioSource audio_1;
+    public AudioSource audio_2;
 
     //////////////////////////////////
+    public void Start(){
+        audio_1.PlayDelayed(audio_1_delay);
+        audio_2.PlayDelayed(audio_2_delay);
+    }
+
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);  
         }
+        
         StartCoroutine("Wait");
        
     }
  
     IEnumerator Wait()
     {
+        
         yield return new WaitForSeconds(second);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
